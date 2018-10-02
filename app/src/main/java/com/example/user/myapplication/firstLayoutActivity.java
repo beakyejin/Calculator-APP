6b479a83a8052bd513512b7bef2be55f8127c170
beakyejin <sse220@naver.com>
2018-10-02 09:36:54 +0900

    계산기, 플레이어

---
+++ app/src/main/java/com/example/user/myapplication/firstLayoutActivity.java
@@ -0,0 +1,37 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class firstLayoutActivity extends AppCompatActivity {
+    private Button dd1, dd2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_layout);
+        init();
+    }
+
+    private void init() {
+        dd1 = findViewById(R.id.btn_1);
+        dd1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //이벤트 연결 확인 toast
+                Toast.makeText(firstLayoutActivity.this, "버튼1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dd2 = findViewById(R.id.btn_2);
+        dd2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(firstLayoutActivity.this, "버튼2", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
