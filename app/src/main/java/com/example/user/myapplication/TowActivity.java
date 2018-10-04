c04b195fc183fc7dce45fda5ce9c70ccc37fd156
beakyejin <sse220@naver.com>
2018-10-04 12:29:32 +0900

    + 계산기 수정(선생님 답안 추가)

--- app/src/main/java/com/example/user/myapplication/TowActivity.java
+++ app/src/main/java/com/example/user/myapplication/TowActivity.java
@@ -3,6 +3,7 @@ package com.example.user.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +21,9 @@ public class TowActivity extends AppCompatActivity {
     }
 
     private void init() {
-        getSupportActionBar().setTitle("계산기");
+        //string.xml 사용해서 타이틀세팅
+        getSupportActionBar().setTitle(R.string.calc);
+
         et_no1 = findViewById(R.id.et_no1);
         et_no1.setShowSoftInputOnFocus(false);
 
@@ -31,12 +34,13 @@ public class TowActivity extends AppCompatActivity {
         tv_result = findViewById(R.id.tv_result);
     }
 
+    //오른쪽(기호) 버튼 클릭 이벤트
     public void clkRightBtn(View v){
         if(v instanceof Button){
             Button b = (Button)v; //강제 형변환
             String str = b.getText().toString();
 
-            if(str.equals("C")){
+            if(str.equals("C")){ //clear
                 et_no1.setText(null);
                 et_no2.setText(null);
                 tv_calc.setText(null);
@@ -51,7 +55,10 @@ public class TowActivity extends AppCompatActivity {
         }
     }
 
+    //왼쪽(숫자)버튼 클릭 이벤트
     public void clkLeftBtn(View v){
+        Log.i("test", "이벤트 연결 성공!"); //로그 찍기
+
         if(v instanceof Button){
             Button b = (Button)v;
             String get_no1 = et_no1.getText().toString();
@@ -62,6 +69,8 @@ public class TowActivity extends AppCompatActivity {
             if (get_calc.getBytes().length > 0) {
                 String str = b.getText().toString();
                 get_no2 += str;
+
+                //계산 완료 시 입력X
                 if(!(get_result.getBytes().length>0)){
                     et_no2.setText(get_no2);
                 }
@@ -70,9 +79,34 @@ public class TowActivity extends AppCompatActivity {
                 get_no1 += str;
                 et_no1.setText(get_no1);
             }
+
+            /*
+            //선생님 답안
+            EditText target = null;
+
+            if(get_calc.equals("")){
+                target = et_no1;
+            }else {
+                target = et_no2;
+            }
+
+            if(target != null){
+                if(v instanceof  Button){
+                    //누른 버튼의 텍스트 값
+                    Button b2 = (Button)v;
+                    String pressNo = b2.getText().toString();
+
+                    //기존에 있던 텍스트 값 + 누른 버튼의 텍스트 값
+                     String existValue = target.getText().toString();
+                     existValue += pressNo;
+
+                     target.setText(existValue);
+                }
+            }*/
         }
     }
 
+    //result(=) 버튼 클릭 이벤트
     public void resultBtn(View v){
         if(v instanceof Button){
             Button b = (Button)v;
@@ -80,13 +114,13 @@ public class TowActivity extends AppCompatActivity {
             int n1, n2;
 
             String get_no1 = et_no1.getText().toString();
-            //값의 길이가 0보다 크면 int형 변환, 0보다 작으면 -1리턴
-            n1 = chkNum(get_no1);
-
             String get_no2 = et_no2.getText().toString();
-            n2 = chkNum(get_no2);
 
             String c = tv_calc.getText().toString();
+
+            //값의 길이가 0보다 크면 int형 변환, 0보다 작으면 -1리턴
+            n1 = chkNum(get_no1);
+            n2 = chkNum(get_no2);
 
             //둘 중에 하나라도 값이 -1이면 계산x(incomplement)
             if((n1 == -1) || (n2 == -1)){
